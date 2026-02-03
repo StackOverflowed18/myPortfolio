f3bd16c13bef1a32e2d9f5249e4a69400187aa88
Anshima singh kushwah <161373731+StackOverflowed18@users.noreply.github.com>
2026-02-04 03:06:32 +0530

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -37,9 +37,9 @@ export const ThemeToggle = () => {
       )}
     >
       {isDarkMode ? (
-        <Sun className="h-6 w-6 text-yellow-300" />
+        <Sun className="h-4 w-4 text-yellow-300" />
       ) : (
-        <Moon className="h-6 w-6 text-blue-900" />
+        <Moon className="h-10 w-4 text-blue-900" />
       )}
     </button>
   );
